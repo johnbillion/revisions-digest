fec04f594a018c67f37fee34eb20af7b6bed3d73
Adam Silverstein <adam@10up.com>
2017-11-23 21:46:20 -0500

    link timeago string to the revision

--- revisions-digest.php
+++ revisions-digest.php
@@ -87,13 +87,14 @@ function widget( $no_idea, array $meta_box ) {
 
 		/* translators: %l: comma-separated list of author names */
 		$changes_by = wp_sprintf(
-			__( 'Changed by %l %s', 'revisions-digest' ),
+			__( 'Changed by %l <a href="%s">%s</a>', 'revisions-digest' ),
 			$authors,
+			get_admin_url( null, 'revision.php?revision=' . $change['latest']->ID ),
 			sprintf( __( '%s ago' ), human_time_diff( $modified_gmt, time() ) )
 		);
 		printf(
 			'<p>%1$s</p>',
-			esc_html( $changes_by )
+			wp_kses_post( $changes_by )
 		);
 
 		echo '<table class="diff">';
